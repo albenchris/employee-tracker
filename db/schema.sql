d1f85c262243bd4f732edd7ee58dea1628753c8d
albenchris <albenchris00@gmail.com>
2021-03-05 09:45:37 -0600

    added roles and employees tables to schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,27 @@
 DROP DATABASE IF EXISTS companyDB;
 
+DROP TABLE IF EXISTS departments;
+
 CREATE DATABASE companyDB;
 
 USE companyDB;
 
 CREATE TABLE departments (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(20) NOT NULL
-)
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INTEGER
+);
